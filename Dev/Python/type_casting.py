f41506c0adf00a301f6398df879c712635a769bc
Sindi298 <sindiswamsubo71@gmail.com>
2024-01-03 18:30:15 +0200

    type_casting

---
+++ Dev/Python/type_casting.py
@@ -0,0 +1,25 @@
+length = len ("Sindi Msubo")
+print("your name has " +str(length) + " characters") #the class length has to be changed to a string class first, because you can't concat 2 different class types
+
+print(type(length))
+
+new_len = str (length)
+print(type(new_len))
+
+#to convert string numbers into int
+print("10" + " 10")
+print(int("10") + int("10"))
+
+#to conver string number to float
+print(10 + float("10"))
+"""
+this will give ValueError
+name = "sindi" 
+print(10 + int(name))
+
+"""
+user1 = input ("input your number: ")
+user2 = input (" input your number: ")
+
+total = (int(user1) + int(user2))
+print(total)
