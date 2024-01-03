d375ad7cbe01e2b550537374d15a0945120c027a
Sindi298 <sindiswamsubo71@gmail.com>
2024-01-03 17:52:39 +0200

    data_types

---
+++ Dev/Python/data_types.py
@@ -0,0 +1,20 @@
+#data types = classes
+#vairables = instances/objects
+#type() function is used to check the data type/class of variable
+
+var_1 = 323
+var_2 = 123.4
+
+print(var_1 + var_2)
+
+print(type(var_1))
+print(type(var_2))
+
+#print(Ob10) #eg; Ob/OB = binary numbers & 10 = 2 in binary form. therefore 2 will be printed
+
+suprise = "Happy New Year "
+year = "2024!!"
+ 
+print(suprise[6])
+print(year[3])
+print(suprise + year)
