f56b9408829ce0e30b3c77fefa289ca20d7ab637
Sindi298 <sindiswamsubo71@gmail.com>
2024-01-03 17:04:20 +0200

    variables

---
+++ Dev/Python/variables.py
@@ -0,0 +1,5 @@
+#there is no need to declare and initialize variables in python, all you need to do is create and use the variable by assigning a value/string etc to it.
+name = input("what is your name? ") #name = varaible name
+print(name)
+length = len(name) #length = varaiable name. len function is used to calculate the length of the input
+print(length)
