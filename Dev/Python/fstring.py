c6439448901a62b338a30d3c9d92cbd6c2e7835c
Sindi298 <sindiswamsubo71@gmail.com>
2024-01-04 13:19:42 +0200

    updated

---
+++ Dev/Python/fstring.py
@@ -0,0 +1,9 @@
+name = 'Holly'
+age = 26
+height = 5.7
+
+#the method below is too tedious, especially if you have a lot of tags.
+print("My name is: ", name, "I am ", age, "years old", "my height is ",height, "meters")
+# if you use (,) instead of (+) then you don't have to typecast
+
+print(f"my name is {name}, I am {age} years old, my height is {height} meters")
