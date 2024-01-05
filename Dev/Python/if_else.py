c104f1988855b6684bd9ef0c3373dcdfd1655e99
Sindi298 <sindiswamsubo71@gmail.com>
2024-01-05 10:12:08 +0200

    ifelse

--- Dev/Python/if_else.py
+++ Dev/Python/if_else.py
@@ -1,7 +1,16 @@
 height = int(input("enter height in meters "))
 
-if(height > 3):
-	print("Token Required")
-	print("You can now board the train")
+if(height >= 3):
+	print("can ride")
+
+	age = int(input("what is your age? "))
+	if (age < 12):
+		print("Please pay R150")
+	elif age <= 18:
+		print("Please pay R250")
+	else:
+		print("Please pay R500")
+
 else:
-	print("No token required")
+	print("Can't ride")
+print("bye")
