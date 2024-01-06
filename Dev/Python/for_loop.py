e38af77ca05ca01f1e502a2557a70491fa7576bb
Sindi298 <sindiswamsubo71@gmail.com>
2024-01-06 10:51:49 +0200

    for_loop

---
+++ Dev/Python/for_loop.py
@@ -0,0 +1,18 @@
+'''
+general syntax of for loop in python =
+for tag_name in sequence:
+	statement(s)
+'''
+
+name = ['Happy New Year', 'sindi'] #this is a list
+for i in name:
+	print(i)
+	if i == 'sindi':
+		print("hey, it's me!")
+
+numbers = [2, 3, 4, 5, 6, 7, 8, 9]
+squares = []
+for p in numbers:
+	square = p ** 2
+	squares.append(square)
+print(squares)
