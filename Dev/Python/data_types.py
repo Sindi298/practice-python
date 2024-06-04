2fba05d21af9ca4d0a3ca14235e19a1419bbb1eb
Sindi298 <sindiswamsubo71@gmail.com>
2024-06-04 11:12:36 +0200

    python data types

--- Dev/Python/data_types.py
+++ Dev/Python/data_types.py
@@ -7,14 +7,16 @@ var_2 = 123.4
 
 print(var_1 + var_2)
 
-print(type(var_1))
-print(type(var_2))
+print(type(var_1)) #resulting class is int
+print(type(var_2)) #resulting class is float
 
 #print(Ob10) #eg; Ob/OB = binary numbers & 10 = 2 in binary form. therefore 2 will be printed
 
 suprise = "Happy New Year "
 year = "2024!!"
  
-print(suprise[6])
-print(year[3])
+print(suprise[6]) #subscript fetches the char stored at location 6. count starts from 0
+print(year[3]) #subscript fetches the char stored at location 3. count starts from 0
 print(suprise + year)
+
+print("jenny\'s \"lectures\"") #black slash skips or ignores the meaning of the special char it is placed before it.
