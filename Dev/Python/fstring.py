fe689e331f3dcc5ac19b7f3ed1adc402fb035e10
Sindi298 <sindiswamsubo71@gmail.com>
2024-07-04 14:28:52 +0200

    practice

--- Dev/Python/fstring.py
+++ Dev/Python/fstring.py
@@ -10,3 +10,17 @@ print(f"my name is {name}, I am {age} years old, my height is {height} meters")
 
 print(f"my name is {name}, I am {age} years old, my height is {height} meters")
 print(f"my name is {name}, and i am a student")
+'''
+Q: WAP to find out how many days, weeks, months
+we have left, if we live until 90 years old?
+'''
+days = 365
+weeks = 52
+months = 12 
+life_expectancy = 90
+years = life_expectancy-age
+
+age = int(input("enter your current age: "))
+
+print(f"you have {years*days} days, {years*weeks} weeks and {years*months} months left to live")
+
