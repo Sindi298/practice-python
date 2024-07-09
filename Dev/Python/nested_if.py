b6295bd29b93bfab58200a546782a3ea45678d29
Sindi298 <sindiswamsubo71@gmail.com>
2024-07-09 20:59:47 +0200

    explanation

---
+++ Dev/Python/nested_if.py
@@ -0,0 +1,20 @@
+#what is nested if? == if inside if. NB: the indentation will identify the level of nesting
+'''
+eg1:if condition1:
+		statement1
+
+			if condition2: --> this is a complete if/else constuct within/inside the 1st if/else construct.
+				statement2a
+			elif condition3:
+				statement3a
+			else:
+				statement4a 
+	
+	else:
+		statement1a
+
+---------------------------------------------------------------------------------------------------------------------------
+
+'''
+
+#what is nested if else? == if the complete if else construct is within either of the (if blocks) or the else block.
