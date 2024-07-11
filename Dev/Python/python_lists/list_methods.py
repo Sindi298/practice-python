ac558f73d534b6e01e426ca24796b817934a279c
Sindi298 <sindiswamsubo71@gmail.com>
2024-07-11 15:48:18 +0200

    new dir

---
+++ Dev/Python/python_lists/list_methods.py
@@ -0,0 +1,18 @@
+#there are 11 list methods/functions in python--read up on them
+'''
+sort()
+pop()
+extend()
+append()
+remove()
+count()
+clear()
+max()
+min()
+len()
+insert()
+index()
+reverse()
+copy()
+
+'''
